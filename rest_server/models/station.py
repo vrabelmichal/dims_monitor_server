7314bb6a393f1d1ec5698010ad9fe382e42c2661
Ing. Michal Vrábel <michal.vrabel@tuke.sk>
2022-03-04 18:02:38 +0100

    added station priority field

--- rest_server/models/station.py
+++ rest_server/models/station.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Station(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    priority = models.IntegerField(default=0)
 
     def __str__(self):
         return f'Station "{self.name}"'
