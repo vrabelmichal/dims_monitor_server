e032123e0d5899dda8ab813c4ef3d08637efed4b
Ing. Michal Vrábel <michal.vrabel@tuke.sk>
2022-03-04 01:08:31 +0100

    Station and EnvironmentLogUpload were added to admin

--- rest_server/admin.py
+++ rest_server/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 from rest_server.models import DiskPartition, DiskUsage, CpuStatus, OhmSensorMeasurement, OhmHardwareInformation, \
-    OhmSensorInformation, OhmSensorParameter, UfoCaptureOutputEntry
+    OhmSensorInformation, OhmSensorParameter, UfoCaptureOutputEntry, EnvironmentLogUpload, Station
 from rest_server.models import Report
 
 admin.site.register(Report)
+admin.site.register(Station)
 admin.site.register(DiskPartition)
 admin.site.register(DiskUsage)
 admin.site.register(CpuStatus)
@@ -13,6 +14,7 @@ admin.site.register(OhmHardwareInformation)
 admin.site.register(OhmSensorInformation)
 admin.site.register(OhmSensorParameter)
 admin.site.register(UfoCaptureOutputEntry)
+admin.site.register(EnvironmentLogUpload)
 
 admin.site.site_header = "DIMS REST Server Administration"
 admin.site.site_title = "DIMS REST Server Administration"
